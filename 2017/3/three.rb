91361ee184171d65e9366ae72e2fb8b6655b8f75
Matthew McPherrin <git@mcpherrin.ca>
2017-12-03 23:52:48 -0800

    part two

--- 2017/3/three.rb
+++ 2017/3/three.rb
@@ -1,15 +1,18 @@
 valid = 0
-invalid = 0
+validwords = 0
 
 File.foreach('input.txt') do |line|
 	words = line.split(" ")
+	anagrams = words.map do |word|
+		word.chars.sort.join
+	end
+	if anagrams.length == anagrams.uniq.length
+		validwords += 1
+	end
 	if words.length == words.uniq.length
 		valid += 1
-	else
-		puts "invalid: #{words}"
-		invalid += 1
 	end
 end
 
 puts "valid: #{valid}"
-puts "invalid: #{invalid}"
+puts "valid anagrams: #{validwords}"
